9962f5f085cb27da8fefdc12e4502e30463207b8
aaron weiner <aaronjacobweiner@yahoo.com>
2021-03-29 13:08:17 -0400

    able to generate html

--- Develope/index.js
+++ Develope/index.js
@@ -2,76 +2,149 @@
 const inquirer = require("inquirer");
 
 const fs = require('fs');
-const generateTemplate = require("./src/page-template");
+const generateTeam = require("./src/page-template");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
+
+const team = [];
+
 // TODO: Create an array of questions for user input
-const questions = [
+const managerQuestions = [
     {
         type: 'input',
-        message: 'What is your projects name?',
-        name: 'title',
+        message: 'What is your managers name?',
+        name: 'name',
     },
     {
         type: 'input',
-        message: 'Write a description.',
-        name: 'description',
-    },    
-    {
-        type: 'input',
-        message: 'Install npm i',
-        name: 'npmi',
-        default: 'npm i',
+        message: 'Whats the id?',
+        name: 'id',
     },
     {
         type: 'input',
-        message: 'Usage?',
-        name: 'usage',
-    },
-    {
-        type: 'list',
-        message: 'What license is this project under?',
-        name: 'license',
-        default: "MIT",
-        choices: ["MIT", "GPLv2", "Apache", "GPLv3", "BSD 3-clause"]
-    },
-    {
-        type: 'input',
-        message: 'What contributions were made?',
-        name: 'contributions',
-    },
-    {
-        type: 'prompt',
-        message: 'To run tests, enter the following command',
-        name: 'npmTest',
-        default: 'npm run test'
-    },
-    {
-        type: 'input',
-        message: 'What is your email?',
+        message: 'What is the email?',
         name: 'email',
     },
     {
         type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'username',
+        message: 'What is the office number?',
+        name: 'officeNumber',
     }
 ];
+
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: "What is your intern's name?",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Whats the id?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is the email?',
+        name: 'email',
+    },
+    {
+        type: "input",
+        message: "What is your school?",
+        name: "school"
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "What is your engineer's name?",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Whats the id?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is the email?',
+        name: 'email',
+    },
+    {
+        type: "input",
+        message: "What is your GitHub account name?",
+        name: "github"
+    }
+]
+
+function addEngineer() {
+    inquirer.prompt(engineerQuestions).then(answers => {
+        console.log(answers);
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        team.push(engineer);
+        addEmployee();
+    })
+
+}
+
+function addIntern() {
+    inquirer.prompt(internQuestions).then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);        
+        team.push(intern);
+        addEmployee();
+    })
+
+}
+
+
+function addEmployee() {
+    const employeePrompt = [{
+        type: 'list',
+        message: 'What type of employee do you want to add?',
+        name: 'memberChoice',
+        choices: ["Intern", "Engineer", "No More Employees"]
+    }];
+
+    inquirer.prompt(employeePrompt)
+        .then(answer => {
+            switch (answer.memberChoice) {
+                case "Intern":
+                    addIntern()
+                    break;
+
+                case "Engineer":
+                    addEngineer()
+                    break;
+
+                default:
+                    writeToFile("output/index.html", team);
+            }
+        })
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
-    fs.writeFile(fileName, generateMarkdown(data), (err) =>
-            err ? console.error(err) : console.log('Success!')
-        );
+    fs.writeFile(fileName, generateTeam(data), (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-        .prompt(questions)
-        .then((response) =>
-            {   console.log(response);
-                writeToFile("?/?", response)}
-            
+        .prompt(managerQuestions)
+        .then((answers) => {
+            console.log(answers);
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+            team.push(manager);
+            addEmployee();
+        }
         )
 }
 
